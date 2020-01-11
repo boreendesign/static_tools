6d58a29f4371dc97b181066ffba91051a6a81ddf
Stephen Morrissey <info@boreendesign.com>
2020-01-11 08:27:38 -0800

    Added create widget form

--- js/agency.js
+++ js/agency.js
@@ -1,6 +1,57 @@
 (function($) {
   "use strict"; // Start of use strict
 
+  function copyToClipboard(elem) {
+  	  // create hidden text element, if it doesn't already exist
+      var targetId = "_hiddenCopyText_";
+      var isInput = elem.tagName === "INPUT" || elem.tagName === "TEXTAREA";
+      var origSelectionStart, origSelectionEnd;
+      if (isInput) {
+          // can just use the original source element for the selection and copy
+          target = elem;
+          origSelectionStart = elem.selectionStart;
+          origSelectionEnd = elem.selectionEnd;
+      } else {
+          // must use a temporary form element for the selection and copy
+          target = document.getElementById(targetId);
+          if (!target) {
+              var target = document.createElement("textarea");
+              target.style.position = "absolute";
+              target.style.left = "-9999px";
+              target.style.top = "0";
+              target.id = targetId;
+              document.body.appendChild(target);
+          }
+          target.textContent = elem.textContent;
+      }
+      // select the content
+      var currentFocus = document.activeElement;
+      target.focus();
+      target.setSelectionRange(0, target.value.length);
+
+      // copy the selection
+      var succeed;
+      try {
+      	  succeed = document.execCommand("copy");
+      } catch(e) {
+          succeed = false;
+      }
+      // restore original focus
+      if (currentFocus && typeof currentFocus.focus === "function") {
+          currentFocus.focus();
+      }
+
+      if (isInput) {
+          // restore prior selection
+          elem.setSelectionRange(origSelectionStart, origSelectionEnd);
+      } else {
+          // clear temporary content
+          target.textContent = "";
+      }
+      return succeed;
+  }
+
+
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
@@ -25,6 +76,15 @@
     target: '#mainNav',
     offset: 56
   });
+
+  $('#createInternalUrl').click(function() {
+    var $title = $('#internal_widget_title').val();
+    var $url = $('#internal_widget_url').val();
+    var code = '{{< insertcontent url="'+$url+'" text="'+$title+'" >}}';
+    $('#internal_widget_code').val(code);
+    copyToClipboard(document.getElementById("internal_widget_code"));
+  });
+
 
   // Collapse Navbar
   var navbarCollapse = function() {
